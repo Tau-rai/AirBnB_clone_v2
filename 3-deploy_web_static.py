cbc7d80c4b23c65cac79298a6ae1f242e718e36a
Tau <taumasaire@gmail.com>
2024-03-07 17:43:13 +0200

    corrections

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@ to my servers using Fabric
 
 
 import os
-from fabric.api import local, run, put, env, sudo
+from fabric.api import *
 from datetime import datetime
 
 
@@ -33,41 +33,38 @@ def do_deploy(archive_path):
     """Distributes an archive to the web servers"""
     if not os.path.exists(archive_path):
         return False
-    try:
-        # copy archive to server
-        put(archive_path, "/tmp/")
 
-        # extract the filename without extension
-        filename = archive_path.split("/")[-1]
-        name = filename.split(".")[0]
+    # copy archive to server
+    put(archive_path, "/tmp/")
 
-        # define the release directory using the extracted name
-        release_dir = f"/data/web_static/releases/{name}/"
+    # extract the filename without extension
+    filename = archive_path.split("/")[-1]
+    name = filename.split(".")[0]
 
-        # uncompress the archive to a folder
-        run(f"mkdir -p {release_dir}")
-        run(f"tar -xzf /tmp/{filename} -C {release_dir}")
+    # define the release directory using the extracted name
+    release_dir = f"/data/web_static/releases/{name}/"
 
-        # remove archive
-        run(f"rm /tmp/{filename}")
+    # uncompress the archive to a folder
+    run(f"mkdir -p {release_dir}")
+    r = run(f"tar -xzf /tmp/{filename} -C {release_dir}")
 
-        # move files into the right directory
-        run(f"mv {release_dir}/web_static/* {release_dir}")
-        run(f"rm -rf {release_dir}/web_static")
-
-        # set appropriate permissions for files and directories
-        sudo(f"chmod -R 755 {release_dir}")
-        sudo(f"chown -R www-data:www-data {release_dir}")
-
-        # delete symbolic link from web server
-        run("rm -rf /data/web_static/current")
-
-        # create a symlink to new version of code
-        run(f"ln -s {release_dir} /data/web_static/current")
-        return True
-    except Exception as e:
-        print(f"Error: {e}")
+    if r.failed:
         return False
+
+    # remove archive
+    # run(f"rm /tmp/{filename}")
+
+    # move files into the right directory
+    run(f"mv -n {release_dir}/web_static/* {release_dir}")
+    run(f"rm -rf {release_dir}/web_static")
+
+    # delete symbolic link from web server
+    run("rm -rf /data/web_static/current")
+
+    # create a symlink to new version of code
+    run(f"ln -s {release_dir} /data/web_static/current")
+
+    return True
 
 
 def deploy():
