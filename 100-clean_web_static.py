a6dda125ada6591dac48c66b77090381926b3972
Tau <taumasaire@gmail.com>
2024-03-07 21:52:46 +0200

    clean up script

---
+++ 100-clean_web_static.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This file contains a script that deletes out-of-date archives
+"""
+from fabric.api import env, run
+from os import listdir, remove
+from os.path import isfile, join
+
+
+env.user = 'ubuntu'
+env.hosts = ['34.204.95.239', '54.234.17.205']
+
+
+def do_clean(number=0):
+    """Deletes out-of-date archives"""
+    number = int(number)
+    if number < 1:
+        number = 1
+
+    # Delete unnecessary archives in the versions folder
+    files = [f for f in listdir('versions') if isfile(join('versions', f))]
+    files.sort(reverse=True)
+    for file in files[number:]:
+        remove(join('versions', file))
+
+    # Delete unnecessary archives on the servers releases folder
+    cmd_list = 'ls -t /data/web_static/releases'
+    cmd_tail_h = 'tail -n +{} |'.format(number + 1)
+    cmd_tail_b = 'xargs -I {{}} rm -rf /data/web_static/releases/{{}}'
+    run(f"{cmd_list} | {cmd_tail_h} {cmd_tail_b}")
