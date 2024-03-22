5fbce02e4dd943476c84ac53203c0dbab4041069
Tau <taumasaire@gmail.com>
2024-03-22 11:56:53 +0200

    number route script

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+This file contains a script that starts a Flask web application,
+that listens on 0.0.0.0 port 5000
+"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays a string"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays a string"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """Displays C folowed by some text"""
+    return f"C { text.replace('_', ' ') }"
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text="is cool"):
+    """Displays Python folowed by some text"""
+    return f"Python { text.replace('_', ' ') }"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """Displays a number n id n is an integer"""
+    return f"{ n } is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
